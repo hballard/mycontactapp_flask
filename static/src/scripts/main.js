ff9782cb6f9040465bbd552059f0206da7ec3a52
Heath Ballard <heath.ballard@gmail.com>
2016-03-04 14:18:14 -0600

    "Modified webpack config and related files"

--- static/src/scripts/main.js
+++ static/src/scripts/main.js
@@ -1,1 +1,2 @@
+require('../styles/main.scss');
 console.log('hello world ');
--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,14 +1,16 @@
 var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin= require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: './src/scripts/main.js',
+    entry: ['./src/scripts/main.js'],
     output: {
-        path: path.resolve(__dirname, './dist/scripts'),
-        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        filename: 'scripts/[name].js',
         publicPath: 'http://localhost:3000/'
     },
     resolve: {
-        extensions: ['', '.js', '.jsx', '.less']
+        extensions: ['', '.js', '.jsx', '.scss']
     },
     devtool: 'source-map',
     // proxy: {
@@ -22,12 +24,18 @@ module.exports = {
             {
                test: [/\.js$/, /\.es6$/],
                exclude: /node_modules/,
-               loader: 'babel',
-               query: {
-                 cacheDirectory: true,
-                 presets: ['react', 'es2015']
-               }
+               loader: 'babel?presets[]=react,presets[]=es2015'
+            },
+            {
+                test: /(\.scss|\.css)$/,
+                loader: ExtractTextPlugin.extract('css!sass')
             }
         ]
     },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new ExtractTextPlugin(path.join('styles','main.css'), {
+            allChunks: true
+        })
+    ]
 };
