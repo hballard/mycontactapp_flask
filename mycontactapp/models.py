79e948c942c7976a1076a1ea85de410dc6ff91a4
hballard <heath.ballard@gmail.com>
2016-03-18 21:59:13 -0500

    Lot's of changes to templates, css, and views

--- mycontactapp/models.py
+++ mycontactapp/models.py
@@ -8,14 +8,14 @@ class Contacts(db.Model):
     last_name = db.Column(db.String(80))
     job_title = db.Column(db.String(120))
     company = db.Column(db.String(120))
-    phone_number = db.Column(db.String(120), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    phone_number = db.Column(db.String(120))
+    email = db.Column(db.String(120))
     address1 = db.Column(db.String(250))
     city = db.Column(db.String(120))
     state = db.Column(db.String(80))
     zipcode = db.Column(db.Integer)
     comments = db.Column(db.Text)
-    active_status = db.Column(db.Boolean)
+    active_status = db.Column(db.Boolean, default=True)
 
     def __init__(self, first_name, last_name, **kwargs):
 
--- mycontactapp/views.py
+++ mycontactapp/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, abort
+from flask import render_template, abort, redirect, url_for
 from .models import Contacts
 from . import app
 
@@ -9,13 +9,24 @@ def page_not_found(e):
 
 
 @app.route('/')
+def landing():
+    all_contacts = Contacts.query.add_columns(Contacts.id, Contacts.first_name,
+                                              Contacts.last_name)\
+                                              .order_by(Contacts.first_name)\
+                                              .first()
+    return redirect('/{}'.format(all_contacts[0].id))
+
+
 @app.route('/<int:contact_id>')
 def index(contact_id=1):
-    all_contacts = Contacts.query.all()
+    all_contacts = Contacts.query.add_columns(Contacts.id, Contacts.first_name,
+                                              Contacts.last_name)\
+                                              .order_by(Contacts.first_name)\
+                                              .all()
     if contact_id > len(all_contacts) or contact_id == 0:
         abort(404)
     else:
-        contact = all_contacts[contact_id - 1]
+        contact = Contacts.query.filter_by(id=contact_id).first()
         return render_template('index.html',
                                contact=contact,
                                all_contacts=all_contacts)
